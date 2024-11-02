584a3d3e6276ec8d3a7c44708e526db1fd02649e
IBlank1zI <67780986+IBlank1zI@users.noreply.github.com>
2024-11-02 13:02:03 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,53 @@
+#Task 1
+from audioop import reverse
+from operator import truediv
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+    if i < 5:
+        print(i)
+
+#task 2
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+set1 = set(a)
+set2 = set(b)
+common_elements = set1.intersection(set2)
+print(list(common_elements))
+
+#Task 3
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_d = dict(sorted(d.items(), key=lambda item: item[1]))
+sorted_d_r = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+print(sorted_d)
+print(sorted_d_r)
+
+#task 4
+dict_a = {1:10, 2:20}
+dict_b = {3:30, 4:40}
+dict_c = {5:50, 6:60}
+
+dicta = dict(dict_a) | dict(dict_b) | dict(dict_c)
+print(dicta)
+
+#Task 5
+my_dict = {'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}
+top_keys = sorted(my_dict, key=my_dict.get, reverse=True)[:3]
+print(top_keys)
+
+#Task 6
+a = int(input('Enter a number: '))
+print(str(a))
+print(type(a))
+
+
+#Task 7
+n = 6
+triangle = []
+for i in range(n):
+    row = [1] * (i + 1)
+    for j in range(1, i):
+        row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    triangle.append(row)
+for row in triangle:
+    print(" " * (n - len(row)), " ".join(map(str, row)))
